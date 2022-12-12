b7d4d49b00198ae1bb78542b5257f7a37cd35375
Elias Furtado <eliasfurtado.dev@gmail.com>
2022-12-11 21:11:52 -0300

    adicionada getHorasNoturnas na ApiController

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Services\HorasService;
 use Illuminate\Http\Request;
+
 
 class ApiController extends Controller
 {
     public function getHorasNoturnas(Request $request) {
-        return 'teste';
+        $inicio = $request->inicio;
+        $fim = $request->fim;
+        
+        $horasService = new HorasService();
+        return $horasService->horasService($inicio, $fim);
+       
+         
     }
 }
